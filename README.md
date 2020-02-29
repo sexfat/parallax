71ecde5ec5b98f96b3c249bc5a355c8441b846b0
sexfat <bryant6831@gmail.com>
2020-02-29 12:51:48 +0800

    add 初始化

---
+++ README.md
@@ -0,0 +1,5 @@
+# 環境介紹
+
+  <h5>環境安裝</h5>
+        <div>下載parallax.min.js</div>
+        <div>scripts.js</div>
---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+//作用場景
+function parallax() {
+    var scene =  document.getElementById('parallax_area');
+    var parallax = new Parallax(scene);
+}
+parallax();
+
+
+function parallax1() {
+    var scene =  document.getElementById('parallax_area1');
+    var parallax = new Parallax(scene);
+}
+
+parallax1();
